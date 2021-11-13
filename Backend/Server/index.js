ae20ddbb2df7e7ccababe23d31a7372d691df36d
SaarSkittel <saar23042gmail.com>
2021-11-13 11:28:09 +0200

    13.11.21
    - Changed DNS container configuration file
    - React frontend app creation

--- Backend/Server/index.js
+++ Backend/Server/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 
-app.get("/", (req, res) => res.send("hellow world!!!"));
+app.get("/", (req, res) => res.json({ message: "Hello world!!!" }));
 const port = process.env.PORT || 80;
 app.listen(80, () => console.log(`Listening on port ${port}`));
