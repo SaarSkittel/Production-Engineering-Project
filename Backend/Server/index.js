300fd4f16fd343e42036930ac9d59c0352f08d0b
SaarSkittel <saar23042gmail.com>
2021-11-15 14:19:02 +0200

    15.11.21
    - changed containers ports
    - communication between front end to backend
    -fixed CORS error

--- Backend/Server/index.js
+++ Backend/Server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-
-
-app.get("/", (req, res) => res.json({ message: "Hello world!!!" }));
 const port = process.env.PORT || 80;
-app.listen(80, () => console.log(`Listening on port ${port}`));
+app.get("/", (req, res) =>
+    res.json({ message: `Hello world from port ${port}!!!` })
+);
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- Frontend/website/src/App.js
+++ Frontend/website/src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [data, setData]= React.useState();
+  const [data, setData] = React.useState();
   React.useEffect(() => {
-    fetch("http://172.28.0.2:80/")
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-  },[]);
+    fetch("http://localhost:8001/")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data.message);
+        console.log(data.message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {!data? "Loading...":data}
-        </p>
-      
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
