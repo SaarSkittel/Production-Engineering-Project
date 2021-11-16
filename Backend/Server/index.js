0bf4bf56fed4046edf0159b6dc636f09558e05cd
SaarSkittel <saar23042gmail.com>
2021-11-16 22:15:27 +0200

    MySQL configuration
    - Configure Database table
    - Set container with persistent data

--- Backend/Server/index.js
+++ Backend/Server/index.js
@@ -4,4 +4,11 @@ const port = process.env.PORT || 80;
 app.get("/", (req, res) =>
     res.json({ message: `Hello world from port ${port}!!!` })
 );
+app.post("/register");
+app.post("/changePassword");
+app.get("/users");
+app.get("/users/name?");
+app.get("/users/id?");
+app.get("/login");
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
